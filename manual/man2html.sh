e8ff2962a9fb364fda6e8de06b91b603cf06b5bc
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2025-09-04 04:41:47 -0300

    chore: Remade hack for generating manual pages.

--- manual/man2html.sh
+++ manual/man2html.sh
@@ -5,37 +5,69 @@
 progname="$(basename $0)"
 
 main(){
-	case $1 in
-		generate) generate ;;
-		nuke) nuke ;;
+	op="$1"; shift
+	case "$op" in
+		generate) generate "$1" "$2";;
+		nuke) nuke "$1" ;;
 		\?|*) print_help ;;
 	esac
 }
 
 generate(){
-	for manual in man*/*.*; do	
-		# Generate HTML version of each manual page, then delete the original
-		# [g,n,t]roff version
+	src="${1:-.}"
+	destdir="${2:-.}"
+
+	for manual in "$src/man"*/*.*; do
+		man_name="${manual##*/}"
+		man_dir="$(basename "$(dirname $manual)")"
+		dest="$destdir/$man_dir"
+		if [ ! -d "$dest" ]; then
+			mkdir -p "$dest" || exit 1
+		fi
 		[ -z "$(cat "$manual.html" 2>/dev/null)" ] \
-		&& mandoc -T html -O style=/assets/mandoc.css "$manual" > "$manual.html" \
-		&& rm -f "$manual"
-		
+		&& mandoc -T html -O style=/assets/mandoc.css "$manual" > "$dest/$man_name.html" 
+	done
+	unset man_name man_dir dest
+
+	for man_dir in man*; do
+		# In case if mathces anything that isn't a directory.
+		[ ! -d $man_dir ] && continue
+
+		dest="$destdir/$man_dir.html"
+
+		# Determine which is the volume description. 
+		case "${man_dir#man*}" in
+			1b) vol="BSD System Compatibility";;
+			1) vol="User Commands";;
+			5) vol="File Formats";;
+			7) vol="Headers, Tables and Macros";;
+			1m|8) vol="Maintenance Commands" ;;
+		esac
+
 		# Generate HTML T.O.C. page for each category
-		_name="${manual#*/}"; name="${_name%%.*}"; unset _name
-		description="$(awk -F' - ' '/id="NAME"/{ getline; print $2; }' "$manual.html")"
-		printf 'Writing %s to %s ...\n' "$name" "${manual%/*}.html" 1>&2 
-		printf '<li><a href="%s">%s</a> -  %s</li>\n' \
-		       	"$manual.html" "$name" "$description" >> "${manual%/*}.html"
+		printf '<html>\n\t<head>\n\t<title>%s</title>\n\t<link\n\t\ttype="text/css"\n\t\trel="stylesheet"\n\t\thref="../assets/style.css">\n\t</head>\n\t<body>\n\t\t<div id='main'>\n\t\t\t<h1>%s</h1>\n\t\t\t<ul>\n' "$vol" "$vol" > "$dest" 
+		for manpage in "$destdir/$man_dir"/*.html; do
+			_name="${manpage##*/}"; name="${_name%%.*}"; unset _name
+
+			# Ugly hack for obtaining the description for a manual
+			# page.
+			description="$(nawk '/id="NAME"/{getline; gsub(/<p.*- /, "", $0); gsub(/<\/p>/, "", $0); print $0}' "$manpage")"
+			printf 'Writing %s to %s ...\n' "$name" "$dest" 1>&2 
+			printf '\t\t\t\t<li><a href="%s">%s</a> -  %s</li>\n' \
+			       	"$manpage" "$name" "$description" >> "$dest"
+		done
+		# Close the HTML document.
+		printf '\t\t\t</ul>\n\t\t</div>\n\t</body>\n</html>' >> "$dest"
 	done
 }
 
 nuke(){
-	find . \( -type f -o -type d \) -name 'man*' \
+	find "${1:-'.'}" \( -type f -o -type d \) -name 'man*' \
 		! \( -name '*.sh' -o -name 'index.html' \) -exec rm -rf {} \;
 }
 
 print_help(){
-	printf 'usage: %s [generate|nuke]\n' $progname 1>&2
+	printf 'usage: %s generate <origin>|nuke [dest]\n' $progname 1>&2
 }
 
-main $1
+main "$@"
